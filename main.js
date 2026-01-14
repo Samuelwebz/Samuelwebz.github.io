200a5b1f9aee2c1a5dd4e78747680d5eebce0216
Samuelwebz <arilhilmika1@gmail.com>
2026-01-14 21:13:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Lekas Sembuh Pinky Girl').split('')
+    const titles = ('Lekas Sembuh Meila ❤').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
